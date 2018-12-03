20a09f4bd75e354c391f7ffa6f72d0aab1a1ce63
Jakub Argasiński <jakub@argasinski.eu>
2018-12-03 16:10:20 +0100

    First two sections on competitions and rules

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var launchDate = moment('2018-12-03T16:00:00+01:00').toDate();
-
-var timerId = countdown(
-  launchDate,
-  function(ts) {
-    document.getElementById('pageTimer').innerHTML = ts.toHTML("span");
-  },
-  countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS
-);
